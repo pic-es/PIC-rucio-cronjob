78f684207ad8e708768a75d4255279ef0c24ea84
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-11-30 01:23:03 +0100

    Fixed issues and build latest image for the docker hub

---
+++ docker/fts/renew_fts_delegation.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+#
+# fts_proxy
+#
+# Create an x509 proxy from the host's igtf grid certificate and delegate
+# against the osg fts server at https://fts01.pic.es:8446.  Run this script (as
+# root) as a cronjob to maintain an FTS-delegated proxy for rucio operations.
+#
+## Configuration
+ftsproxylog=/var/log/fts-proxy.log
+proxytool=/usr/bin/voms-proxy-init 
+hostcert=/etc/grid-security/usercert.pem
+hostkey=/etc/grid-security/userkey.pem
+x509proxy=/tmp/x509up_u0
+ftsdelegate=/bin/fts-rest-delegate
+ftsserver=https://fts01.pic.es:8446
+
+# export PASSPHRASE="if needed"
+
+## Logging info
+dtstamp="`date +%F-%A-%H.%M.%S `"
+echo -e "\n################ ${dtstamp} ################" >> ${ftsproxylog}
+
+## Create robot proxy
+echo -e "${dtstamp}: ${proxytool} -cert ${hostcert} -key ${hostkey} -out ${x509proxy} -debug "
+echo ${proxytool} -cert ${hostcert} -key ${hostkey} -out  ${x509proxy} -pwstdin -debug 2>> ${ftsproxylog} 2>&1
+proxy_ret=$?
+echo -e "${dtstamp}: ${proxytool} return: ${proxy_ret}\n" >> ${ftsproxylog} 2>&1
+
+${proxytool} -cert ${hostcert} -key ${hostkey} -out  ${x509proxy} -pwstdin -debug 2>> ${ftsproxylog} 2>&1
+
+## Delgate proxy
+echo "here i'm"
+echo -e "${ftsdelegate} -f -v -s ${ftsserver} --cert ${x509proxy} --key ${x509proxy}"  >> ${ftsproxylog} 2>&1
+
+${ftsdelegate} -f -v -s ${ftsserver} --cert ${x509proxy} --key ${x509proxy}  >> ${ftsproxylog} 2>&1
+
+delegate_ret=$? 
+echo -e "${dtstamp}: ${ftsdelegate} return: ${delegate_ret}" >> ${ftsproxylog} 2>&1
+
+
