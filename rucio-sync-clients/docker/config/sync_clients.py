e8daa2cfd28011ca7c06b1ceee6d00ac1db8f5fc
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-12-04 18:58:46 +0100

    Fixed issues

--- rucio-sync-clients/docker/config/sync_clients.py
+++ rucio-sync-clients/docker/config/sync_clients.py
@@ -4,29 +4,18 @@ import json
 import sys
 import traceback
 
-from rucio.api.vo import add_vo
+#from rucio.api.vo import add_vo
 from rucio.client import Client
 from rucio.common.config import config_get, config_get_bool
 from rucio.common.exception import Duplicate
-from rucio.core.account import add_account_attribute
+#from rucio.core.account import add_account_attribute
 from rucio.common.types import InternalAccount
 
 
 if __name__ == '__main__':
-    if config_get_bool('common', 'multi_vo', raise_exception=False, default=False):
-        vo = {'vo': config_get('client', 'vo', raise_exception=False, default='tst')}
-        try:
-            add_vo(new_vo=vo['vo'], issuer='super_root', description='A VO to test multi-vo features', email='N/A', vo='def')
-        except Duplicate:
-            print('VO {} already added'.format(vo['vo']) % locals())
-    else:
-        vo = {}
-
+    
     # parameters
-    if argv:
-        rse_repo_file = argv[0]
-    else:
-        rse_repo_file = '/rse_repository.json'
+    rse_repo_file = '/account_repository.json'
     json_data = open(rse_repo_file)
     repo_data = json.load(json_data)
     json_data.close()
@@ -34,3 +23,8 @@ if __name__ == '__main__':
     c = Client()
     for client in repo_data:
         print(client)
+        #try:
+        #    c.add_account(client, 'SERVICE', '')
+        #except Duplicate:
+        #    print('Account {} already added'.format(client))
+
