97e9bc9ac4cf687700b342297317d45497907b66
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-12-10 12:18:05 +0100

    Fixed issued with the clients synchronization

--- rucio-sync-rses/docker/config/sync_rses.py
+++ rucio-sync-rses/docker/config/sync_rses.py
@@ -4,111 +4,76 @@ import json
 import sys
 import traceback
 
+#from rucio.api.vo import add_vo
 from rucio.client import Client
+from rucio.common.config import config_get, config_get_bool
 from rucio.common.exception import Duplicate
+#from rucio.core.account import add_account_attribute
+from rucio.common.types import InternalAccount
 
 
-UNKNOWN = 3
-CRITICAL = 2
-WARNING = 1
-OK = 0
-
-fts_pic = "https://fts01.pic.es:8446"
-
-def main(argv):
+if __name__ == '__main__':
+    
     # parameters
-    if argv:
-        rse_repo_file = argv[0]
-    else:
-        rse_repo_file = '/rse_repository.json'
-
+    rse_repo_file = '/account_repository.json'
     json_data = open(rse_repo_file)
     repo_data = json.load(json_data)
     json_data.close()
 
     c = Client()
-    for rse in repo_data:
-        print(rse)
-        try:
-            if "deterministic" in repo_data[rse]:
-                deterministic = repo_data[rse].get('deterministic', True)
-            elif "deterministic" in repo_data[rse] and repo_data[rse]["deterministic"] == False :
-                deterministic = repo_data[rse].get('deterministic', False)
-            else : 
-                deterministic = repo_data[rse].get('deterministic', True)
-            volatile = repo_data[rse].get('volatile', False)
-            region_code = repo_data[rse].get('region_code')
-            country_name = repo_data[rse].get('country_name')
-            staging_area = repo_data[rse].get('staging_area')
-            continent = repo_data[rse].get('continent')
-            time_zone = repo_data[rse].get('time_zone')
-            ISP = repo_data[rse].get('ISP')
-            print()
-            print('Trying to add {} rse'.format(rse)) 
-            c.add_rse(rse, deterministic=deterministic, volatile=volatile, region_code=region_code, country_name=country_name, staging_area=staging_area, continent=continent, time_zone=time_zone, ISP=ISP)
-            print(deterministic, volatile, region_code, country_name, staging_area, continent, time_zone, ISP) 
-        except Duplicate:
-            print('%(rse)s already added' % locals())
-        except:
-            errno, errstr = sys.exc_info()[:2]
-            trcbck = traceback.format_exc()
-            print('Interrupted processing with %s %s %s.' % (errno, errstr, trcbck))
-        for p_id in repo_data[rse]['protocols']['supported']:
-            try:
-                print(repo_data[rse]['protocols']['supported'][p_id])
-                p = repo_data[rse]['protocols']['supported'][p_id]
-                p['scheme'] = p_id
-                print(p)
-                c.add_protocol(rse, p)
-            except ValueError as e:
-                print(rse, e)
-            except Duplicate as e:
-                print(rse, e)
-            except Exception:
-                errno, errstr = sys.exc_info()[:2]
-                trcbck = traceback.format_exc()
-                print('Interrupted processing for %s with %s %s %s.' % (rse, errno, errstr, trcbck))
-        try:
-           c.add_rse_attribute(rse, key="fts", value=fts_pic)
-           print("Successfully added fts {} to RSE {}".format(fts_pic, rse))
-        except: 
-           errno, errstr = sys.exc_info()[:2]
-           trcbck = traceback.format_exc()
-           print('Interrupted processing with %s %s %s.' % (errno, errstr, trcbck))
-        try:
-           # c.set_rse_limits(rse, name="root", value=-1)
-           c.set_local_account_limit(account='root', rse=rse, bytes=-1)
-           print("Successfully locally set limits for RSE {}".format(rse))
-        except:
-           errno, errstr = sys.exc_info()[:2]
-           trcbck = traceback.format_exc()
-           print('Interrupted processing with %s %s %s.' % (errno, errstr, trcbck))
+    for client in repo_data:
+        print(client)
         
+        if 'email' in repo_data[client]: 
+            email = repo_data[client]['email']
+        else :
+            email = 'bruzzese@pic.es'
+        
+        # it is mandatory to have an email 
+        if email == None :
+            email = 'bruzzese@pic.es'
+
         try:
-           # c.set_rse_limits(rse, name="root", value=-1)
-           c.set_global_account_limit(account='root', rse_expression=rse, bytes=-1)
-           print("Successfully globaly set limits for RSE {}".format(rse))
-        except:
-           errno, errstr = sys.exc_info()[:2]
-           trcbck = traceback.format_exc()
-           print('Interrupted processing with %s %s %s.' % (errno, errstr, trcbck))
+            c.add_account(client, 'SERVICE', email)
+        except Duplicate:
+            print('Account {} already added'.format(client))
 
-        rses = c.list_rses()
-        for src in rses:
-           for dst in rses:
-               print("Trying to set distance between RSE {} and {}".format(src['rse'], dst['rse']))
+        try:
+            scope = 'test-' + str(client)
+            c.add_scope(client, scope)
+        except Duplicate:
+            print('Scope {} already added'.format(scope))
+
+        print(list(c.list_identities(client)))
+        if 'x509' in repo_data[client]:
+            for id in repo_data[client]['x509'] :
                try:
-                  c.add_distance(source=src['rse'], destination=dst['rse'], parameters={"ranking":1, "distance":1})
-                  print("Successfully added distance between RSEs {} and {}".format(rse, dst['id']))
-               except Duplicate:
-                  print('%(rse)s already added' % locals())
-               except:
-                  errno, errstr = sys.exc_info()[:2]
-                  trcbck = traceback.format_exc()
-                  print('Interrupted processing with %s %s %s.' % (errno, errstr, trcbck))
+                   c.add_identity(account=client, identity=id, authtype='x509', email=email)
+               except Exception:
+                   print('Already added: ', id)
 
+        if 'GSS' in repo_data[client]:
+            for id in repo_data[client]['GSS'] :
+               try:
+                   c.add_identity(account=client, identity=id, authtype='GSS', email=email)
+               except Exception:
+                   print('Already added: ', id)
 
-if __name__ == '__main__':
-    main(sys.argv[1:])
+        if 'GSS' in repo_data[client]:
+            for id in repo_data[client]['GSS'] :
+                try:
+                   c.add_identity(account=client, identity=id, authtype='GSS', email=email)
+                except Exception:
+                   print('Already added: ', id)
 
+        if 'USERPASS' in repo_data[client]:
+            user = repo_data[client]['USERPASS'][0] 
+            pswd = repo_data[client]['USERPASS'][1]
+            print(client, user, pswd, email)
+            try:
+                c.add_identity(account=client, identity=user, authtype='userpass', password=pswd, email=email)            
+            except Duplicate :
+                print('Already added: ', user)
+
+        print(list(c.list_identities(client)))
 
