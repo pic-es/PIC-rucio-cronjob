320b91e2a77376da5989a6318e477d7ee71166ef
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-12-14 12:39:36 +0100

    Added examples on how to start the docker image

---
+++ rucio-pic-client/docker/client.sh
@@ -0,0 +1,2 @@
+#!/bin/bash
+docker run -e RUCIO_ACCOUNT=root -e RUCIO_CFG_AUTH_TYPE=userpass -e RUCIO_CFG_USERNAME=ddmlab -e RUCIO_CFG_PASSWORD=secret -it -d --name=rucio-client bruzzese/rucio-client
