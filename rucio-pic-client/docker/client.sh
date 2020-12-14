d009b2d5e1ebdcbc43fcc03d0c2f4089633aa470
BruzzeseAgustin <bruzzese.agustin@gmail.com>
2020-12-14 13:32:31 +0100

    Fixed problems with version

--- rucio-pic-client/docker/client.sh
+++ rucio-pic-client/docker/client.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
+docker pull bruzzese/rucio-client:latest
 docker run -e RUCIO_ACCOUNT=root -e RUCIO_CFG_AUTH_TYPE=userpass -e RUCIO_CFG_USERNAME=ddmlab -e RUCIO_CFG_PASSWORD=secret -it -d --name=rucio-client bruzzese/rucio-client
 
